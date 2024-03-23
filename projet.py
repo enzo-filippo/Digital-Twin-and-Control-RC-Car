2efae5a80cce57d17000976c74492631cd2650db
enzo-filippo <eng.enzofilippo@gmail.com>
2024-03-23 09:36:32 +0100

    deleted:    Model.py deleted:    __pycache__/model.cpython-311.pyc deleted:    animation.py deleted:    compound.py deleted:    control.py deleted:    delta.png deleted:    doc.py deleted:    kinematics.py deleted:    main.py deleted:    model.py deleted:    projet.py deleted:    psi.png deleted:    sadasd.py deleted:    sim.dat deleted:    sim.png deleted:    simx.png deleted:    simy.png deleted:    throttle.png

--- projet.py
+++
@@ -1,8 +0,0 @@
-## Archive principal - exec du projet
-
-class car:
-    def __init__(self, init_x: float, init_y: float, init_yaw: float, px: float, py: float, pyaw: float, delta_time: float):
-        self.x = init_x
-        self.y = init_y
-        self.yaw = yaw
-        self.x
