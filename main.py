03de2a445e7c8ec0677327665fae937e285b2262
enzo-filippo <eng.enzofilippo@gmail.com>
2024-03-23 10:55:28 +0100

    modified:   .gitignore 	modified:   main.py 	modified:   model.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import model
 
+# ODE Solver parameters
+abserr = 1.0e-8
+relerr = 1.0e-6
+stoptime = 10
+numpoints = 1000
+t = model.np.linspace(0,stoptime,numpoints)
+ode_param = [abserr, relerr, stoptime, numpoints]
+
 # Parameter values
 max_steer_angle = 30.0
 m = 0.04
@@ -14,16 +22,9 @@ C_alpha = 0.9
 Fz = m*9.98/4
 min_v = 0.1
 throttle2omega = (1/255)*10
-alpha_0 = 0.0
-
-# ODE Solver parameters
-abserr = 1.0e-8
-relerr = 1.0e-6
-stoptime = 15
-numpoints = 1000
+param = [max_steer_angle, m, Iz, lf, lr, Lw, r, mi, C_s, C_alpha, Fz, min_v, throttle2omega]
 
 # Initial conditions
-# x1 and x2 are the initial displacements; y1 and y2 are the initial velocities
 x0 = 0.0
 y0 = 0.0
 psi0 = 0.0
@@ -36,17 +37,8 @@ psipp0 = 0.0
 Xep0 = 0.0
 Yep0 = 0.0
 var_delta = 0.0
-
-# Create the time samples for the output of the ODE solver.
-# I use a large number of points, only because I want to make
-# a plot of the solution that looks nice.
-t = model.np.linspace(0,stoptime,numpoints)
-throttle_0 = 255
-
-# Pack up the parameters and initial conditions:
-p = [max_steer_angle, m, Iz, lf, lr, Lw, r, mi, C_s, C_alpha, Fz, min_v, throttle2omega, alpha_0, throttle_0]
 val_0 = [x0, y0, psi0, xp0, xpp0, yp0, ypp0, psip0, psipp0, Xep0, Yep0, var_delta]
-ode_p = [abserr, relerr, stoptime, numpoints]
-voiture = model.NonLinearBycicle(p, val_0)
-voiture.run(t, ode_p)
+
+voiture = model.NonLinearBycicle("curva_t_255_d_20", param, val_0)
+voiture.run(t, ode_param)
 voiture.plot()
--- model.py
+++ model.py
@@ -2,12 +2,12 @@ import numpy as np
 from scipy.integrate import odeint
 from pylab import show, figure, plot, xlabel, ylabel, grid, legend, title, savefig, axis
 from matplotlib.font_manager import FontProperties
-
-counter = 0
+import os
 
 # non-linear lateral bicycle model
 class NonLinearBycicle():
-    def __init__(self, parameters, val_0):
+    def __init__(self, name, parameters, val_0):
+        self.name = name
         self.max_steer = np.radians(parameters[0])   
         self.m = parameters[1]             
         self.Iz = parameters[2]            
@@ -21,8 +21,6 @@ class NonLinearBycicle():
         self.Fz = parameters[10]
         self.min_v = parameters[11]
         self.throttle2omega = parameters[12]
-        self.alpha_0 = parameters[13]
-        self.throttle_0 = parameters[14]
 
         self.x = val_0[0]
         self.y = val_0[1]
@@ -36,41 +34,36 @@ class NonLinearBycicle():
         self.Xe_p = val_0[9]
         self.Ye_p = val_0[10]
         self.delta_p = val_0[11]
-        
-        #print(self.x_p)
-        
-        self.delta_old = self.delta_p
 
         self.f_w = wheel(self.lf, 0.0, self.mi, self.r, self.mi, self.C_s, self.C_alpha, self.Fz, self.throttle2omega)
         self.r_w = wheel(0.0, self.lr, self.mi, self.r, self.mi, self.C_s, self.C_alpha, self.Fz, self.throttle2omega)
-        # self.f_w.update(self.throttle_0, self.delta_p, self.psi_p, self.x_p, self.y_p, True, self.alpha_0)
-        # self.r_w.update(self.throttle_0, 0.0, self.psi_p, self.x_p, self.y_p, True, self.alpha_0)
+
+        if not os.path.exists('results'):
+            os.makedirs('results')
+        if not os.path.exists(os.path.join('results',self.name)):
+            os.makedirs(os.path.join('results',self.name))
 
     def run(self, t, ode_p):
-        global counter
-        counter = 0
         abserr, relerr, _, _ = ode_p
-  
-        #self.x_pp = (self.f_w.Fx + self.r_w.Fx)/self.m + self.psi_p*self.y_p
-        #self.y_pp = (self.f_w.Fy + self.r_w.Fy)/self.m + self.psi_p*self.x_p
-        #self.psi_pp = (self.lf*self.f_w.Fy - self.lr*self.r_w.Fy)/self.Iz
-        #self.delta_p = self.delta_old - delta
-        #self.delta_old = delta
 
         coef = [self.f_w, self.r_w, self.m, self.Iz]
         val_0 = [self.x_p, self.x_pp, self.y_p, self.y_pp, self.psi_p, self.psi_pp, self.Xe_p, self.Ye_p]
 
         # Call the ODE solver.
-        wsol = odeint(vectorfield, val_0, t, args=(coef,),
-                    atol=abserr, rtol=relerr)
+        wsol = odeint(vectorfield, val_0, t, args=(coef,),atol=abserr, rtol=relerr)
+        
+        self.f_w.Xe = wsol[:,6] + self.f_w.lf*np.cos(wsol[:,4])
+        self.f_w.Ye = wsol[:,7] + self.f_w.lf*np.sin(wsol[:,4])
+        self.r_w.Xe = wsol[:,6] - self.r_w.lr*np.cos(wsol[:,4])
+        self.r_w.Ye = wsol[:,7] - self.r_w.lr*np.sin(wsol[:,4])
 
-        with open('sim.dat', 'w') as f:
-            # #Print & save the solution.
-            for t1, w1 in zip(t, wsol):
-                print(t1, w1[0], w1[1], w1[2], w1[3], w1[4], w1[5], w1[6], w1[7], file=f)
+
+        with open(os.path.join('results',self.name,'sim.dat'), 'w') as f:
+            for t1, w1, xef, yef, xer, yer in zip(t, wsol, self.f_w.Xe, self.f_w.Ye, self.r_w.Xe, self.r_w.Ye):
+                print(t1, w1[0], w1[1], w1[2], w1[3], w1[4], w1[5], w1[6], w1[7], xef, yef, xer, yer, file=f)
 
     def plot(self):
-        t, x, xp, y, yp, psi, psip, Xe, Ye = np.loadtxt('sim.dat', unpack=True)
+        t, x, xp, y, yp, psi, psip, Xe, Ye, Xef, Yef, Xer, Yer = np.loadtxt(os.path.join('results',self.name,'sim.dat'), unpack=True)
 
         figure(1, figsize=(6, 4.5))
         xlabel('t')
@@ -79,7 +72,7 @@ class NonLinearBycicle():
         lw = 1
         # axis('equal')
         plot(t, x, 'b', linewidth=lw)
-        savefig('simx.png', dpi=100)
+        savefig(os.path.join('results',self.name,'simx.pdf'), dpi=100)
         
         figure(2, figsize=(6, 4.5))
         xlabel('t')
@@ -88,7 +81,7 @@ class NonLinearBycicle():
         lw = 1
         # axis('equal')
         plot(t, y, 'r', linewidth=lw)
-        savefig('simy.png', dpi=100)
+        savefig(os.path.join('results',self.name,'simy.pdf'), dpi=100)
         
         figure(3, figsize=(6, 4.5))
         xlabel('Xe')
@@ -97,35 +90,34 @@ class NonLinearBycicle():
         lw = 1
         axis('equal')
         plot(Xe, Ye, 'k', linewidth=lw)
-        savefig('sim.png', dpi=100)
+        savefig(os.path.join('results',self.name,'sim.pdf'), dpi=100)
 
         figure(4, figsize=(6, 4.5))
-        xlabel('t')
-        ylabel('delta')
-        grid(True)
-        lw = 1
-        axis('equal')
-        plot(t, delta(t), 'k', linewidth=lw)
-        savefig('delta.png', dpi=100)
-        
-        figure(5, figsize=(6, 4.5))
-        xlabel('t')
-        ylabel('throttle')
-        grid(True)
-        lw = 1
-        axis('equal')
-        plot(t, throttle(t), 'k', linewidth=lw)
-        savefig('throttle.png', dpi=100)
-
-        figure(6, figsize=(6, 4.5))
         xlabel('t')
         ylabel('psi')
         grid(True)
         lw = 1
         axis('equal')
         plot(t, psi, 'k', linewidth=lw)
-        savefig('psi.png', dpi=100)
+        savefig(os.path.join('results',self.name,'psi.pdf'), dpi=100)
 
+        figure(5, figsize=(6, 4.5))
+        xlabel('t')
+        ylabel('delta')
+        grid(True)
+        lw = 1
+        plot(t, self.f_w.delta(t), 'k', linewidth=lw)
+        savefig(os.path.join('results',self.name,'delta.pdf'), dpi=100)
+        
+        figure(6, figsize=(6, 4.5))
+        xlabel('t')
+        ylabel('throttle')
+        grid(True)
+        lw = 1
+        plot(t, self.f_w.throttle(t), 'k', linewidth=lw)
+        savefig(os.path.join('results',self.name,'throttle.pdf'), dpi=100)
+
+        show()
 
 class wheel():
     def __init__(self, lf, lr, Lw, r, mi, C_s, C_alpha, Fz, throttle2omega):
@@ -143,15 +135,10 @@ class wheel():
         if self.lr == 0:
             self.name = "f_w"
 
-    def update(self, throttle, delta, psi_p, x_p, y_p, initialization, alpha_0):
-        #print(psi_p, x_p, y_p)
+    def update(self, throttle, delta, psi_p, x_p, y_p):
         omega = self.throttle2omega*throttle
-        #print(self.name,"omega:", omega)
-        #print(self.name,"throttle:", throttle)
         Vx = x_p
         Vy = y_p
-        #print(self.name,"Vx:",Vx)
-        #print(self.name,"Vy:",Vy)
         
         if throttle > 0:
             s = (self.r*omega - Vx)/(self.r*omega)
@@ -165,48 +152,39 @@ class wheel():
 
         numerador = Vy + self.lf * psi_p - self.lr * psi_p
         denominador = Vx + signal*self.Lw/2 * psi_p
-        #print(self.name,"num:",numerador)
-        #print(self.name,"den:",denominador)
         
         theta_V = np.arctan(numerador/denominador) 
         if np.isnan(theta_V):
             theta_V = 0
-        #print(self.name,"theta_V:",theta_V)
         alpha = delta - theta_V
-
-        #print(self.name,"alpha:",alpha)
 
         C_lambda = (self.mi * self.Fz*(1+s))/(2*np.sqrt((self.C_s*s)**2 + (self.C_alpha*np.tan(alpha)**2)))
         if C_lambda < 1:
             f_C_lambda = (2-C_lambda)*C_lambda
         else:
             f_C_lambda = 1 
-        
-        #print(self.name,"C_lambda:",C_lambda)
-        #print(self.name,"s:",s)
 
         Fxp = self.C_s *(s/(1+s))*f_C_lambda
         Fyp = self.C_alpha*(np.tan(alpha)/(1+s))*f_C_lambda
 
         self.Fx = Fxp*np.cos(delta) - Fyp*np.sin(delta)
         self.Fy = Fxp*np.sin(delta) + Fyp*np.cos(delta)
-        #print(self.name,"Fx, Fy: ", self.Fx, self.Fy)
 
-def throttle(t):
-    return t - t + 255
+    def throttle(self,t):
+        valor = t*0 + 255
+        return valor
 
-def delta(t):
-    angulo = 20
-    multiplicador = 1/360
-    t1 = 2.5
-    x1 = t - t1
-    t2 = 5
-    x2 = t - t2
+    def delta(self,t):
+        angulo = 20
+        multiplicador = 1/360
+        t1 = 2.5
+        x1 = t - t1
+        t2 = 5
+        x2 = t - t2
 
-    # valor = (np.sin(2*np.pi*(1/10)*(t-t0)) +1)
-    valor_rad = np.heaviside(x1,1) - np.heaviside(x2,1)
-    valor_deg = multiplicador*angulo*valor_rad
-    return valor_deg
+        valor_rad = np.heaviside(x1,1) - np.heaviside(x2,1)
+        valor_deg = multiplicador*angulo*valor_rad
+        return t*0 + valor_deg
 
 def vectorfield(w, t, coef):
     """
@@ -222,15 +200,8 @@ def vectorfield(w, t, coef):
     x, xp, y, yp, psi, psip, Xe, Ye = w
     f_w, r_w, m, Iz = coef
 
-    global counter 
-    global delta
-    global throttle
-    f_w.update(throttle(t), delta(t), psip, xp, yp, False, 0.0)
-    r_w.update(throttle(t), 0.0, psip, xp, yp, False, 0.0)
- 
-    counter = counter + 1
-    #print("counter:", counter)
-    #print("xp =",xp)
+    f_w.update(f_w.throttle(t), f_w.delta(t), psip, xp, yp)
+    r_w.update(r_w.throttle(t), 0.0, psip, xp, yp)
 
     Fxf = f_w.Fx
     Fyf = f_w.Fy
@@ -248,8 +219,6 @@ def vectorfield(w, t, coef):
          (lf*Fyf - lr*Fyr)/Iz,
          xp*np.cos(psi)-yp*np.sin(psi),
          xp*np.sin(psi)+yp*np.cos(psi)]
-    
-    #print(f)
     
     return f
     
