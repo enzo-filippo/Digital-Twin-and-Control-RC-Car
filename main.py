dfb5f0dabff9ede25461afd6bc1e159b407fd137
enzo-filippo <eng.enzofilippo@gmail.com>
2024-04-29 14:47:54 +0200

    deleted:    anim.py 	modified:   main.py 	deleted:    main100.py 	deleted:    main120.py 	deleted:    model.py 	new file:   rccar.py 	modified:   real_data.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-import model
+import rccar
 import real_data
 
-# ODE Solver parameters
+
+sim_file_directory = "curva_t_255_d_20"
+exp_file_directory = "data"
+exp_file_name = "120.txt"
+
+# ODE Solver parameters - INPUTS FOR EVERY ANALYSIS
 abserr = 1.0e-8
 relerr = 1.0e-6
-stoptime = 7.9
-numpoints = 81
-step = numpoints/stoptime
-t = model.np.linspace(0,stoptime,numpoints)
-ode_param = [abserr, relerr, stoptime, numpoints]
+initial_time = 3
 
-# Parameter values
+# Getting the real data value and the time date to make the simulation
+treal, tsim, stoptime, numpoints, xreal, yreal, vreal, areal, t_max, length, t0, Xe0, Ye0, v0, a0, psi0_tout_droit = rccar.read_exp_file(exp_file_directory, exp_file_name, initial_time)
+# Variable Parameter values
+mi = 0.59
+C_s = 0.11
+C_alpha = 0.9
+
+# Fixed Parameter values
 max_steer_angle = 30.0
 m = 0.04
 Iz = 0.000266
@@ -18,46 +26,48 @@ lf = 0.047
 lr = 0.05
 Lw = 0.0
 r = 0.024
-mi = 0.57
-C_s = 0.9
-C_alpha = 0.9
 Fz = m*9.98/4
-min_v = 0.1
-throttlereal = 110
-throttle = -(throttlereal - 127)
-throttle2omega = 51.66/r
-param = [max_steer_angle, m, Iz, lf, lr, Lw, r, mi, C_s, C_alpha, Fz, min_v, throttle2omega]
+throttle2omega = 0.05166/r
+
+# Simulation conditions
+throttle_real_command = 120
+initial_time_throttle = 0
+final_time_throttle = 100
+throttle_type = "step"
+delta_real_command = 0
+initial_time_delta = 0
+final_time_delta = stoptime
+delta_type = "straight"
 
 # Initial conditions
-x0 = 0.0
-y0 = 0.0
-psi0 = model.np.radians(-27.667+180)
-xp0 = 0.0001 #must not be zero
+x0 = Xe0
+y0 = Ye0 
+psi0 = psi0_tout_droit + rccar.np.pi # in rad
+xp0 = 0.0
 xpp0 = 0.0
 yp0 = 0.0
 ypp0 = 0.0
 psip0 = 0.0
 psipp0 = 0.0
-Xe0 = 3.4285081
-Ye0 = 0.1956809
+#Xe0 = 3.6077931899999998 # real values that comes from the experimental
+#Ye0 = 1.3031706500000002 # real values that comes from the experimental
 var_delta = 0.0
 val_0 = [x0, y0, psi0, xp0, xpp0, yp0, ypp0, psip0, psipp0, Xe0, Ye0, var_delta]
 
-voiture = model.NonLinearBycicle("curva_t_255_d_20", param, val_0)
-x, y = voiture.run(t, ode_param)
-# voiture.plot()
-voiture.anim(step)
+# Packaging the parameters
+#t = rccar.np.linspace(0,stoptime,numpoints)
+ode_param = [abserr, relerr, stoptime, numpoints]
+throttle_sim = -(throttle_real_command - 127)
+throttle_parameters = rccar.set_throttle(throttle_sim, initial_time_throttle, final_time_throttle, throttle_type)
+print(throttle_parameters)
+delta_parameters = rccar.set_delta(delta_real_command, initial_time_delta, final_time_delta, delta_type)
+print(delta_parameters)
+param = [max_steer_angle, m, Iz, lf, lr, Lw, r, mi, C_s, C_alpha, Fz, throttle2omega, throttle_parameters, delta_parameters]
+print(val_0)
+voiture = rccar.NonLinearBycicle(sim_file_directory, param, val_0)
+voiture.run(tsim, ode_param)
 
+tsimu, xsimu, xpsimu, ysimu, ypsimu, psi, psip, Xe, Ye, Xef, Yef, Xer, Yer = rccar.read_sim_file(sim_file_directory)
 
-real_data.plt.figure(figsize=(6, 4.5))
-real_data.plt.xlabel("y [m]")
-real_data.plt.ylabel("x [m]")
-real_data.plt.grid(True)
-real_data.plt.axis('equal')
-lw = 1
-real_data.plt.plot(real_data.run(throttlereal).x, real_data.run(throttlereal).y,'r:', label ="real")
-real_data.plt.plot(x, y, 'b:', label ="sim")
-real_data.plt.legend()
-real_data.plt.show()
-
-
+# PLOTS
+rccar.ComparisonPlot(treal, xreal, yreal, vreal, tsim, Xe, Ye, xpsimu, ypsimu, exp_file_name)
