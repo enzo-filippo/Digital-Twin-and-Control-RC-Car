82c33a0ff164c47b273377fb1ab6baa9c7674d0f
enzo-filippo <eng.enzofilippo@gmail.com>
2024-05-05 15:49:57 +0200

    deleted:    main.py 	modified:   main100.py 	modified:   main105_191.py 	modified:   main105_63.py 	modified:   main110.py 	modified:   main120.py 	modified:   main90.py 	modified:   main_acceleration_and_break.py 	modified:   main_acceleration_and_turn.py

--- main105_191.py
+++ main105_191.py
@@ -1,4 +1,4 @@
-import rccar2
+import rccar
 
 # Real data and simulation:
 throttle_real_command = 105
@@ -14,7 +14,7 @@ relerr = 1.0e-6
 initial_time = 2.2
 
 # Getting the real data value and the time date to make the simulation
-treal, tsim, stoptime, numpoints, xreal, yreal, vreal, areal, t_max, length, t0, Xe0, Ye0, v0, a0, psi0_tout_droit = rccar2.read_exp_file(exp_file_directory, exp_file_name, initial_time)
+treal, tsim, stoptime, numpoints, xreal, yreal, vreal, areal, t_max, length, t0, Xe0, Ye0, v0, a0, psi0_tout_droit = rccar.read_exp_file(exp_file_directory, exp_file_name, initial_time)
 mi = 0.7
 C_s = 0.61
 C_alpha = 0.27
@@ -43,7 +43,7 @@ delta_type = "step"
 # Initial conditions
 x0 = Xe0
 y0 = Ye0 
-psi0 = rccar2.np.arctan(-0.772)+rccar2.np.pi # in rad
+psi0 = rccar.np.arctan(-0.772)+rccar.np.pi # in rad
 xp0 = 0.0
 xpp0 = 0.0
 yp0 = 0.0
@@ -56,24 +56,24 @@ var_delta = 0.0
 val_0 = [x0, xp0, y0, yp0, psi0, psip0, Xe0, Ye0]
 
 # Packaging the parameters
-#t = rccar2.np.linspace(0,stoptime,numpoints)
+#t = rccar.np.linspace(0,stoptime,numpoints)
 ode_param = [abserr, relerr, stoptime, numpoints]
 throttle_sim = -(throttle_real_command - 127)
-throttle_parameters = rccar2.set_throttle(throttle_sim, initial_time_throttle, final_time_throttle, throttle_type)
+throttle_parameters = rccar.set_throttle(throttle_sim, initial_time_throttle, final_time_throttle, throttle_type)
 print(throttle_parameters)
 delta_sim = ((delta_real_command-127)*max_steer_angle)/127
-delta_parameters = rccar2.set_delta(delta_sim, initial_time_delta, final_time_delta, delta_type)
+delta_parameters = rccar.set_delta(delta_sim, initial_time_delta, final_time_delta, delta_type)
 print(delta_parameters)
 param = [max_steer_angle, m, Iz, lf, lr, Lw, r, mi, C_s, C_alpha, Fz, throttle2omega, throttle_parameters, delta_parameters]
 print(val_0)
-voiture = rccar2.NonLinearBicycle(sim_file_directory, param, val_0)
+voiture = rccar.NonLinearBicycle(sim_file_directory, param, val_0)
 voiture.run(tsim, ode_param)
 
-tsimu, xsimu, xpsimu, ysimu, ypsimu, psi, psip, Xe, Ye,  xef1, yef1, xer1, yer1, xef2, yef2, xer2, yer2, tv, dv, s_f, s_r = rccar2.read_sim_file(sim_file_directory)
+tsimu, xsimu, xpsimu, ysimu, ypsimu, psi, psip, Xe, Ye,  xef1, yef1, xer1, yer1, xef2, yef2, xer2, yer2, tv, dv, s_f, s_r = rccar.read_sim_file(sim_file_directory)
 
 # PLOTS
-rccar2.run_all_animations(sim_file_directory, fps=10)
-rccar2.ComparisonPlot(treal, xreal, yreal, vreal, tsim, Xe, Ye, xpsimu, ypsimu, tv, dv, s_f, s_r, exp_file_name)
+rccar.run_all_animations(sim_file_directory, fps=10)
+rccar.ComparisonPlot(treal, xreal, yreal, vreal, tsim, Xe, Ye, xpsimu, ypsimu, tv, dv, s_f, s_r, exp_file_name)
 
 
 
